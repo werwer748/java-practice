e268febbde763875c51a4ff289cb18a1684d124b
hugo <werwer748@naver.com>
2023-11-23 23:59:24 +0900

    first-commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Main에서 java 프로그램이 시작된다.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("안녕 자바!");
+    }
+
+    /*
+    * psvm: public static void main(String[] args) {} 자동생성
+    * sout: System.out.println("안녕 자바!"); 자동입력
+    */
+}
---
+++ src/sec02/chap02/Main.java
@@ -0,0 +1,25 @@
+package sec02.chap02;
+
+// 파일명과 클래스명이 일치해야 한다.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // 스파이더맨;
+        // System.out.println("떠라 떠라 떠라");
+
+        System.out.println("안녕 세상아!");
+
+        //한줄
+        System.out.println("주석은 비슷비슷"); // 여기도 가능
+
+        /*
+        * 여러줄 주석도 똑같이
+        * 하하하하하하하
+        * 이렇게 하면 됩니다~
+        * 우효효
+        *
+        * 주석의 양은 실행속도에 영향이 없다!
+        * 그래도 주석없이 알아보기 쉬운 코드를 짜자*/
+    }
+}
