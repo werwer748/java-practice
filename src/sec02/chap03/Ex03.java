532f4aaf65a81f7d475ec1a88b7070d640a7bf48
hugo <werwer748@naver.com>
2023-11-26 17:21:00 +0900

    java 기초

---
+++ src/sec02/chap03/Ex03.java
@@ -0,0 +1,23 @@
+package sec02.chap03;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        String 대학 = "학생";
+
+        // 일반적으로는 아래와 같이 선언과 초기화를 동시에
+        int age = 20;
+        boolean isMarried = false;
+        double height = 179.99;
+        char sex = 'M';
+        String name = "홍길동";
+
+        // 중간중간에 브레이크포인트를 찍고 디버그 모드를 쓰는 버릇을 들이자
+        //  변수 - 값을 변경할 수 있다는 의미
+        age = 21;
+
+//        age = false;
+//        isMarried = "안함";
+//        sex = 1.2
+//        name = 3;
+    }
+}
---
+++ src/sec02/chap03/Ex05.java
@@ -0,0 +1,24 @@
+package sec02.chap03;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        //  💡 final 연산자 : 변수의 값을 바꾸지 못하게 만듦
+
+        // 상수 보다는 한번만 값을 넣을 수 있는 주머니라고 생각하자.
+        final int INT_NUM = 1;
+//        INT_NUM = 2; // ⚠️ 불가
+
+        //  사용 가능
+        int yalco, _yalco, $yalco, 얄코;
+
+        // 사용 불가
+//        int 1yalco;
+//        int yal co;
+//        int #yalco;
+
+        // 사용 불가 - 예약어들.
+//        int double;
+//        boolean instanceof;
+//        char new;
+    }
+}
